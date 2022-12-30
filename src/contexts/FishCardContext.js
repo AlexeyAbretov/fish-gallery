3bea45b55505fcfe927514bd57aa6889331f9643
Alexey Abretov <aleksey.abretov@gazprombank.ru>
2022-12-30 15:16:30 +0300

    feat(reducer): Интеграция редьюсера
    meta: task-19

--- src/contexts/FishCardContext.js
+++ src/contexts/FishCardContext.js
@@ -2,11 +2,12 @@
 import React from 'react';
 
 export const FishCardContextInitialValues = {
-    liked: [],
-    deleted: [],
-    cards: [],
-    setDeleted: () => {},
-    setLiked: () => {}
+    state: {
+        liked: [],
+        deleted: [],
+        cards: [],
+    },
+    dispatch: () => { },
 }
 
 export const FishCardContext = React.createContext(FishCardContextInitialValues);
